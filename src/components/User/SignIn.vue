9cd4765821c56329ab64af14951022b7e6902110
Prady <pradnyeshmali19@gmail.com>
2021-04-09 22:27:47 +0530

    Firebase & Sign UP page

--- src/components/User/SignIn.vue
+++ src/components/User/SignIn.vue
@@ -1,10 +1,96 @@
 <template>
-  <h2>Sign In Page</h2>
+  
+  <v-form ref="form" v-model="valid" lazy-validation>
+  <v-row>
+    <v-col xs="12" sm="12" md="6" offset-md="3">
+        <v-text-field
+        v-model="name"
+        :counter="10"
+        :rules="nameRules"
+        label="Name"  
+        required
+      ></v-text-field>
+    </v-col>
+    
+    <v-col xs="12" sm="12" md="6" offset-md="3">
+        <v-text-field
+          v-model="email"
+          :rules="emailRules"
+          label="E-mail"
+          required
+      ></v-text-field>
+    </v-col>
+
+    <v-col xs="12" sm="12" md="6" offset-md="3">
+        <v-text-field
+          v-model="password"
+          :rules="passRules"
+          label="Password"
+          required
+      ></v-text-field>
+    </v-col>
+
+    <v-col xs="12" sm="12" md="6" offset-md="3">
+        <v-text-field
+          v-model="confirmPassword"
+          :rules="[comparePass]"
+          label="Confirm Password"
+          required
+      ></v-text-field>
+    </v-col>
+
+
+    <v-col xs="12" sm="12" md="6" offset-md="3">
+        <v-btn
+      :disabled="!valid"
+      color="success"
+      class="mr-4"
+      @click="onSignUp"
+      type="submit"
+    >
+      SignUp
+    </v-btn>
+    </v-col>
+    
+
+  </v-row>
+  </v-form>
+
 </template>
 
 <script>
 export default {
+  data: ()=>({
+    valid: true,
+    name:'',
+    nameRules:[
+      v => !!v || 'Name is required',
+      v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+    ],
+    email: '',
+    emailRules: [
+      v => !!v || 'E-mail is required',
+      v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+    
+    password: '',
+    confirmPassword:''
+  }),
 
+  computed:{
+    comparePass(){
+      return this.password !== this.confirmPassword ? 'Password Do not Match' : ''
+    }
+  },
+
+  methods: {
+    onSignUp(){
+      this.$refs.form.validate()
+      // Vuex
+    }
+  }, 
+    
+  
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import {store} from './store/index'
 import vuetify from './plugins/vuetify'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import DateFilter from './filters/date'
+import * as firebase from 'firebase'
 
 Vue.config.productionTip = false
 Vue.filter('date',DateFilter)
@@ -12,7 +13,17 @@ new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  created(){
+    firebase.initializeApp({
+      apiKey: "AIzaSyDOsAcscnElU6RfuOFD0E0ICzLUdROf5JY",
+      authDomain: "meet-ups-e78ee.firebaseapp.com",
+      projectId: "meet-ups-e78ee",
+      storageBucket: "meet-ups-e78ee.appspot.com",
+      messagingSenderId: "916491785874",
+      appId: "1:916491785874:web:e99879baccc4028599384b",
+    })
+  }
 }).$mount('#app')
 
 
